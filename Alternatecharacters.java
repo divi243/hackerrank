5faeaf7649caa237c733b28e5af04bfd777c3ae2
Divita <divitagoswami995@gmail.com>
2020-08-02 09:22:02 +0530

    Create Alternatecharacters.java

---
+++ Alternatecharacters.java
@@ -0,0 +1,50 @@
+//  https://www.hackerrank.com/challenges/alternating-characters
+
+// Solution
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(String s) {
+        char A[] = s.toCharArray();
+        int count = 0;
+        for(int i =0;i<A.length-1;i++){
+              if(A[i] == A[i+1]){
+                  A[i]=0;
+                  count++;
+              }
+        }
+        
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = alternatingCharacters(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
