d8227a4c635e76eac86200a66760594c18225f11
Divita <divitagoswami995@gmail.com>
2020-08-04 08:33:26 +0530

    Update stringvalidation.java

--- stringvalidation.java
+++ stringvalidation.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -9,51 +8,25 @@ import java.util.regex.*;
 
 public class Solution {
 
-    // Complete the isValid function below.
-static String isValid(String s) {
-    int[] a = new int[26];
-    for(int i=0;i<s.length();i++){
-        int index = s.toCharArray()[i]-97;
-        a[index]++;
-       // System.out.print(index);
-    }
-   Arrays.sort(a);
-   int max = a[a.length-1];
-   int x = 0;
-   for(int i=0;i<a.length;i++){
-       System.out.print(a[i]);
-       if(a[i]==0){
-           x++;
-       }
-   }
-   int min =0;
-   
-  
-   System.out.println(max);
-  
-    min = a[x];
-  
-    System.out.println(min);
-    
-   System.out.println(a[a.length-1]);
+    // Complete the substrCount function below.
+    static long substrCount(int n, String s) {
 
-if(((max - min == 1) && (max > a[24])) || (min == max ) || ( a[a.length-1] == 1 )){
-        return  "YES";
-    }else{
-        return "NO";
+
     }
-}
 
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
         String s = scanner.nextLine();
 
-        String result = isValid(s);
+        long result = substrCount(n, s);
 
-        bufferedWriter.write(result);
+        bufferedWriter.write(String.valueOf(result));
         bufferedWriter.newLine();
 
         bufferedWriter.close();
